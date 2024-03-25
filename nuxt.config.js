73fb0defda0f6e55d7b5b591c9455cf7e75bcc24
Karsten <karstenkwong@gmail.com>
2024-03-25 11:53:04 +0800

    Test

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PCR Tools',
+    title: 'kana39hus.github.io',
     htmlAttrs: {
       lang: 'en'
     },
